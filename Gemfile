d3f4834148afa088a85a6a34e6bf68c125f44395
Sandrine Ayral <sandrine.ayral@gmail.com>
2019-12-09 11:26:31 +0100

    Finish setting up CRUD

--- Gemfile
+++ Gemfile
@@ -9,6 +9,7 @@ gem 'sass-rails', '~> 5'
 gem 'webpacker', '~> 4.0'
 gem 'turbolinks', '~> 5'
 gem 'jbuilder', '~> 2.7'
+gem 'simple_form'
 gem 'bootsnap', '>= 1.4.2', require: false
 
 group :development, :test do
--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -13,6 +13,12 @@ class PostsController < ApplicationController
   end
 
   def create
+    @post = Post.new(post_params)
+    if @post.save
+      redirect_to posts_path
+    else
+      render :new
+    end
   end
 
   def edit
