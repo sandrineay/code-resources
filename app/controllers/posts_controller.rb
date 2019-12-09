5248911a90688443c5b597530d56df3cd429e89c
Sandrine Ayral <sandrine.ayral@gmail.com>
2019-12-09 11:16:05 +0100

    Setup CRUD for posts

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,36 @@
+class PostsController < ApplicationController
+  before_action :find_post, only: [ :show, :edit, :update, :destroy ]
+
+  def index
+    @posts = Post.all
+  end
+
+  def show
+  end
+
+  def new
+    @post = Post.new
+  end
+
+  def create
+  end
+
+  def edit
+  end
+
+  def update
+  end
+
+  def destroy
+  end
+
+  private
+
+  def find_post
+    @post = Post.find(params[:id])
+  end
+
+  def post_params
+    params.require(:post).permit(:title, :link, :description)
+  end
+end
